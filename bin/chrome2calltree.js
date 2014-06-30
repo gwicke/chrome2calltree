b74681c1b0566c28883a3588c7fc8ede242311e4
Gabriel Wicke <wicke@wikidev.net>
2014-06-30 16:23:46 -0700

    Remove execSync dependency

--- bin/chrome2calltree.js
+++ bin/chrome2calltree.js
@@ -24,12 +24,6 @@ parser.addArgument(['-i', '--infile'], {
     'help': "Read chrome CPU profile from INFILE. " +
             "If omitted, reads from standard in."
 });
-parser.addArgument(['-k', '--kcachegrind'], {
-    'help': "Run the kcachegrind tool on the converted data. Unless combined " +
-            "with the -o flag, will write output to a temporary file and " +
-            "remove it after kcachegrind exits.",
-    'action': 'storeTrue'
-});
 
 var args = parser.parseArgs();
 
@@ -48,11 +42,6 @@ if (args.infile) {
 
 if (args.outfile) {
     outStream = fs.createWriteStream(args.outfile);
-} else if (args.kcachegrind) {
-    outStream = temp.createWriteStream({
-        'prefix': 'chrome2calltree',
-        'suffix': '.log'
-    });
 } else {
     outStream = process.stdout;
 }
@@ -69,32 +58,11 @@ var readEntireStream = function(stream, cb) {
     });
 };
 
-var launchKCacheGrind = function(logpath) {
-    var found = false;
-    var commands = ['qcachegrind', 'kcachegrind'];
-    for (var i = 0; i < commands.length; i++) {
-        var cmd = commands[i];
-        if (execSync.exec('which ' + cmd).code === 0) {
-            execSync.run(cmd + ' ' + outStream.path);
-            found = true;
-            break;
-        }
-    }
-    if (!found) {
-        process.stdout.write("Could not find kcachegrind executable. Tried: " +
-                commands.join(", "));
-        process.exit(1);
-    }
-};
-
 readEntireStream(inStream, function(contents) {
     chrome2calltree.chromeProfileToCallgrind(JSON.parse(contents), outStream);
     outStream.on('finish', function() {
         if (outStream !== process.stdout) {
           outStream.close();
-        }
-        if (args.kcachegrind) {
-            launchKCacheGrind();
         }
     });
     if (outStream !== process.stdout) {
